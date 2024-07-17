1f7c045c887d015c223f15cef9da2bce49540936
ltriyadi91 <triyadilukman92@gmail.com>
2024-07-18 05:35:32 +0700

    feat: simple example from mdn

--- app.js
+++ app.js
@@ -1,81 +1,41 @@
-const express = require('express');
-const {resolve, join} = require("path");
+const express = require("express");
+const { resolve, join } = require("path");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', resolve('views'));
-app.use(express.static(resolve("static")))
+app.set("view engine", "ejs");
+app.set("views", resolve("views"));
+app.use(express.static(resolve("static")));
 
-function sleep(mili = 500) {
-    return new Promise((r) => setTimeout(r, mili))
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/stream", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  send(res);
+});
+
+const sendEvent = (res, event, data, id) => {
+  res.write(`id: ${id}\n`)
+  res.write(`event: ${event}\n`);
+  res.write(`data: ${JSON.stringify(data)}\n\n`);
+};
+
+const sendData = (res, data) => {
+  res.write(`data: ${data}\n\n`);
+};
+
+let i = 0
+function send(res) {
+  sendEvent(res, 'userconnect', { username: 'bobby', time: '02:33:48' }, i);
+  sendData(res, `Here's a system message of some kind that will get used`);
+  sendEvent(res, 'usermessage', { username: 'bobby', time: '02:34:11', text: 'Hi everyone.' }, i);
+  i++;
+
+  setTimeout(() => send(res), 1000);
 }
-
-app.get('/', (req, res) => {
-    res.render('index', {title: 'EJS Example', message: 'Hello, world!'});
-});
-
-
-app.post("/api/upload-batch", async (req, res) => {
-
-    // process big step operation
-
-    const locals = req.app.locals
-
-    locals.eventName = "UPLOADING_AREA"
-
-    await sleep()
-    locals.eventName = "UPLOADING_DEPARTMENT"
-
-    await sleep()
-    locals.eventName = "UPLOADING_DESIGNATION"
-
-    await sleep()
-    locals.eventName = "UPLOADING_EMPLOYEE"
-
-    await sleep()
-    locals.eventName = "UPLOADING_SCHEDULE"
-
-    await sleep()
-    locals.eventName = "UPLOADING_DRIVERS"
-
-    await sleep()
-    locals.eventName = "UPLOADING_ASSIGN_CAR"
-
-    await sleep()
-    locals.eventName = "COMPLETED"
-
-})
-
-app.get('/api/events', async (req, res) => {
-
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-
-    const waitForEvent = () => {
-        return new Promise(resolve => {
-            const checkEvent = () => {
-                const eventName = req.app.locals.eventName;
-                res.write(`data: ${eventName}\n\n`);
-                if (eventName === "COMPLETED") {
-                    res.end();
-                    resolve();
-                } else {
-                    setTimeout(checkEvent, 200);
-                }
-            };
-
-            checkEvent();
-        });
-    };
-
-    await waitForEvent();
-
-    return res.end();
-});
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
